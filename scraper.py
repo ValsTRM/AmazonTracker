8a6e430aa8d6b198417573db438dfafa14b0c824
Valaya Choudhary <35052710+ValsTRM@users.noreply.github.com>
2020-09-20 16:30:49 -0400

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1,23 +1,27 @@
+
 import requests
+#you use Beautiful soup to scrape through the amazon's html file and moving it over the 
 from bs4 import BeautifulSoup
 from re import sub
 from decimal import Decimal
 from playsound import playsound
+
 headers = {
-    #"User-Agent": input("Search your user agent on Google. Paste it here: ")
+    
    "User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
 }
 
-
+#User Inputs amazon's url
 URL = input("What item would you like to buy? Paste URL for Amazon item here: ")
-#URL="https://www.amazon.com/EVGA-GeForce-Gaming-Graphics-11G-P4-2487-KR/dp/B07KVKRLG2/ref=sr_1_3?dchild=1&keywords=rtx+2080ti&qid=1600548714&sr=8-3%27"
-
+#Getting data from amazon bring the data and puts it into code
 response = requests.get(URL, headers=headers).content
-
+#is reading through the html of the amazon link
 soup = BeautifulSoup(response, 'html.parser')
 
 def check_price():
+    #User inputs price that he wants to have for the item
     userinput= int(input("What is the target price you want?: "))
+    #get the title of the product
     title = soup.find(id='productTitle').get_text().strip()
     price = soup.find(id='priceblock_ourprice').get_text().strip()
     print("Product name & specs : ", title.strip())
